d0d37627aec7968171946d525a1e2f2acf0133fa
Joshwin Zuidema <jzuidema@e-sites.nl>
2019-07-18 15:23:09 +0200

    Hide elements in select2 template result

--- src/Kunstmaan/AdminBundle/Resources/ui/js/_advanced-select.js
+++ src/Kunstmaan/AdminBundle/Resources/ui/js/_advanced-select.js
@@ -1,11 +1,25 @@
 var kunstmaanbundles = kunstmaanbundles || {};
 
-kunstmaanbundles.advancedSelect = (function(window, undefined) {
+kunstmaanbundles.advancedSelect = (function (window, undefined) {
 
     var init;
 
-    init = function() {
-        $('.js-advanced-select').select2();
+    init = function () {
+        $('.js-advanced-select').select2({
+            templateResult: function (option) {
+                if (!option.element) {
+                    return option.text;
+                }
+
+                var element = $(option.element);
+
+                if (element.css('display') === 'none') {
+                    return false;
+                }
+
+                return option.text;
+            }
+        });
     };
 
     return {
